6d774078899b7032ba84206b9f688ab0ec5db9ad
Son-KnownPain <sonhong443@gmail.com>
2023-08-11 03:59:00 +0700

    Add create form modal

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
-<a id="button-component" class="{{ $type }}">
+<a id="button-component" class="{{ $type }}" data-toggle="modal" data-target=".launch-pricing-modal">
     <span class="text">
         @if (strstr($text, "icon:"))
             <img style="position: relative; right: 0; top: 0;" class="svg-icon" src="{{ substr($text, 5) }}" alt="SVG Icon">
--- resources/views/components/create-form.blade.php
+++ resources/views/components/create-form.blade.php
@@ -1,25 +1,36 @@
-<div class="col-lg-12 col-md-12">
-    <div class="card">
-        <div class="header">
-            <h2>{{ $title }}</h2>
-        </div>
-        <div class="body">
-            @foreach ($columnsName as $clnName)
-                @if (!in_array($clnName, $ignoreColumns))
-                    <div class="input-group input-group-sm mb-3">
-                        <input name="{{$clnName}}" placeholder="Enter {{$clnName}}" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+<div class="modal fade launch-pricing-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" style="display: none;" aria-modal="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content" style="padding-top: 12px;">
+            <div class="col-lg-12 col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>{{ $title }}</h2>
                     </div>
-                @endif
-            @endforeach
-
-            {{-- <x-select2-dropdown></x-select2-dropdown> --}}
+                    <div class="body">
+                        @foreach ($columnsName as $clnName)
+                            @if (!in_array($clnName, $ignoreColumns))
+                                <div class="input-group input-group-sm mb-3">
+                                    <input name="{{$clnName}}" placeholder="Enter {{$clnName}}" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                                </div>
+                            @endif
+                        @endforeach
             
-            <div style="display: flex; justify-content: right; align-items: center; margin-top: 12px;">
-                <x-button type="primary" text="Create"></x-button>
+                        {{-- <x-select2-dropdown></x-select2-dropdown> --}}
+                        
+                        <div style="display: flex; justify-content: right; align-items: center; margin-top: 12px;">
+                            <x-button type="primary" text="Create"></x-button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    const dataSubmit = {};
+    
+</script>
 
 <script>
     $(document).ready(function() {
