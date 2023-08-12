1e068d84a7b8055ecbc8c00507b73e73da2d8d39
Son-KnownPain <sonhong443@gmail.com>
2023-08-12 20:45:03 +0700

    Fix navbar header

--- resources/views/client-pages/contact/index.blade.php
+++ resources/views/client-pages/contact/index.blade.php
@@ -55,7 +55,7 @@
     <!--Contact Page End-->
     <div class="google-map">
         <div class="google-map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.7965155840207!2d105.77957149999999!3d10.033643699999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0881f9a732075%3A0xfa43fbeb2b00ca73!2sCUSC%20-%20Cantho%20University%20Software%20Center!5e0!3m2!1svi!2s!4v1691820584439!5m2!1svi!2s" "></iframe>"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.7965155840207!2d105.77957149999999!3d10.033643699999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a0881f9a732075%3A0xfa43fbeb2b00ca73!2sCUSC%20-%20Cantho%20University%20Software%20Center!5e0!3m2!1svi!2s!4v1691820584439!5m2!1svi!2s" "></iframe></iframe>
          </div>
 
     </div>
