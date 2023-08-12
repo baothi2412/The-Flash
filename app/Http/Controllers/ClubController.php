39d278254b800fdda9bdb2fbc1eaa95ec0b3056c
thanhnguyen2001tlt <ntthanha20158@cusc.ctu.edu.vn>
2023-08-13 02:23:16 +0700

    code

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -5,23 +5,79 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Club;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\JsonResponse;
 
 class ClubController extends Controller
 {
-    public function index(Request $request) {
-        $clubs =\DB::connection()->getSchemaBuilder()->getColumnListing((new Club)->getTable());
+    public function index(Request $request)
+    {
+        $clubs = \DB::connection()->getSchemaBuilder()->getColumnListing((new Club)->getTable());
         $data = Club::all();
         return view('admin-pages.club.index', [
             'title' => 'Players',
-            'records'=>$data,
-            'clubs'=>$clubs,
+            'records' => $data,
+            'clubs' => $clubs,
             'fileColumnsName' => [
                 'Logo'
             ]
         ]);
     }
+    public function detail($id) {
+        $club = Club::find($id);
 
-    public function store(Request $request) {
+        return response()->json([
+            'club' => $club
+        ]);
+    }
+    public function delete($id) {
+        $clubs = Club::find($id);
+        $clubs->delete();
+        return response()->json(
+            [
+                'isSuccess' => true,
+                'message' => 'Successfully delete clubs account',
+                'data' => $clubs
+            ]
+        );
+    }
+    public function update(Request $request):JsonResponse
+    {
+
+        $id = $request->input('id');
+        $logo = $request->file('Logo');
+        $clubName = $request->input('ClubName');
+        $foundedYear = $request->input('FoundedYear');
+        $stadium = $request->input('Stadium');
+        $country = $request->input('Country');
+
+        $club = Club::find($id);
+        $imgName = 'img' . time() . '-' . Str::slug($clubName) . '.' . $logo->extension();
+       
+        $club->logo = $imgName;
+        $club->clubName = $clubName;
+        $club->foundedYear = $foundedYear;
+        $club->stadium = $stadium;
+        $club->country = $country;
+        // Image upload
+        Storage::delete(public_path('images\file-uploads') + $club->logo);
+        $logo->move(public_path('images\file-uploads'), $imgName);
+
+        $club->save();
         
+        return response()->json(
+            [
+                'isSuccess' => true,
+                'message' => 'Successfully uploaded club',
+                'data' => $club
+            ]
+        );
+
+    }
+
+   
+    public function store(Request $request)
+    {
+
     }
 }
--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -16,5 +16,104 @@ class MatchController extends Controller
 
         ]);
     }
+   
+    public function store(Request$request){
+        $id = $request->input('id');
+        $scoringPlayerID = $request->input('ScoringPlayerID');
+        $DateTimeStart = $request->input('DateTimeStart');
+        $dateTimeEnd = $request->input('DateTimeEnd');
+        $stadium = $request->input('Stadium');
+        $teamAID = $request->input('TeamAID');
+        $teamAFormation = $request->input('TeamAFormation');
+        $teamBID = $request->input('TeamBID');
+        $teamBForScoremation = $request->input('TeamBForScoremation');
+        $score = $request->input('Score');
+        $type = $request->input('Type');
+
+        $match = new MyMatch();
+        
+       
+        $match->id = $id;
+        $match->ScoringPlayerID = $scoringPlayerID;
+        $match->DateTimeStart = $DateTimeStart;
+        $match->DateTimeEnd = $dateTimeEnd;
+        $match->Stadium = $stadium;
+        $match->TeamAID = $teamAID;
+        $match->TeamAFormation = $teamAFormation;
+        $match->TeamBID = $teamBID;
+        $match->TeamBForScoremation = $teamBForScoremation;
+        $match->Score = $score;
+        $match->Type = $type;
+        
+
+        $match->save();
+
+        return response()->json(
+            [
+                'isSuccess' => true,
+                'message' => 'Successfully creation match',
+                'data' => $match
+            ]
+        );
+    
+    }
+    public function update(Request $request)
+    {
+        $id = $request->input('id');
+        $scoringPlayerID = $request->input('ScoringPlayerID');
+        $DateTimeStart = $request->input('DateTimeStart');
+        $dateTimeEnd = $request->input('DateTimeEnd');
+        $stadium = $request->input('Stadium');
+        $teamAID = $request->input('TeamAID');
+        $teamAFormation = $request->input('TeamAFormation');
+        $teamBID = $request->input('TeamBID');
+        $teamBForScoremation = $request->input('TeamBForScoremation');
+        $score = $request->input('Score');
+        $type = $request->input('Type');
+
+        $match = MyMatch::find($id);
+
+       
+        $match->ScoringPlayerID = $scoringPlayerID;
+        $match->DateTimeStart = $DateTimeStart;
+        $match->DateTimeEnd = $dateTimeEnd;
+        $match->Stadium = $stadium;
+        $match->TeamAID = $teamAID;
+        $match->TeamAFormation = $teamAFormation;
+        $match->TeamBID = $teamBID;
+        $match->TeamBForScoremation = $teamBForScoremation;
+        $match->Score = $score;
+        $match->Type = $type;
+
+        $match->save();
+        
+        return response()->json(
+            [
+                'isSuccess' => true,
+                'message' => 'Successfully uploaded match',
+                'data' => $match
+            ]
+        );
+
+    }
+    public function delete($id){
+        $match = MyMatch::find($id);
+        $match->delete();
+
+        return response()->json(
+            [
+                'isSuccess' => true,
+                'message' => 'Successfully delete match',
+                'data' => $match
+            ]
+        );
+    }
+    public function detail($id) {
+        $myMatch = MyMatch::find($id);
+
+        return response()->json([
+            'myMatch' => $myMatch
+        ]);
+    }
 
 }
